be729881b1f7e2c28a9ca8e8d7b63501fb905af9
Ryan Archer <rlarcher@andrew.cmu.edu>
2016-08-31 13:02:02 -0400

    Changed what the main method printed and added another method to print stuff

--- Hello.java
+++ Hello.java
@@ -1,6 +1,10 @@
 
 public class Hello {
   public static void main(String[] args) {
-    System.out.println("Hello, World");
+    System.out.println("Hello there, World");
   }
+}
+
+public void printStuff() {
+  System.out.println("Stuff");
 }
